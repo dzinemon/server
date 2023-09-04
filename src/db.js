87cc44e6e7651433d8b13f905e7e7ef0095a3666
Andrii S <andriisharapatov@gmail.com>
2023-09-04 23:09:26 +0300

    db

--- src/db.js
+++ src/db.js
@@ -11,11 +11,7 @@ const poolConfigLocal = {
 }
 
 const poolConfigVercel = {
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  // port: 5432
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 }
 
 const pool = new Pool(useLocal ? poolConfigLocal : poolConfigVercel)
